7389c6d299ddb8d250bae3a06aedbb2d74874c57
John Bommarito <marp@testServer>
2024-02-18 12:26:50 -0500

    Changed equipory to adopt costume system.  Costume system will allow for costume type items to be equipped in a new costume tab of the equipory to allow for costume items to have a clothing overlay on top of existing clothing items.

--- src/haven/Equipory.java
+++ src/haven/Equipory.java
@@ -30,58 +30,64 @@ import java.util.*;
 import static haven.Inventory.sqlite;
 import static haven.Inventory.sqlo;
 
-public class Equipory extends Widget implements DTarget {
-    static Coord ecoords[] = {
-	new Coord(250, 0),
-	new Coord(50, 70),
-	new Coord(250, 70),
-	new Coord(300, 70),
-	new Coord(50, 0),
-	new Coord(50, 210),
-	new Coord(25, 140),
-	new Coord(275, 140),
+public class Equipory extends Widget {
+    public static final Box[] boxen = {
+	new Box(new Coord(250,   0), Resource.loadtex("gfx/hud/inv/head"),   0),
+	new Box(new Coord( 50,  70), Resource.loadtex("gfx/hud/inv/face"),   0),
+	new Box(new Coord(250,  70), Resource.loadtex("gfx/hud/inv/shirt"),  0),
+	new Box(new Coord(300,  70), Resource.loadtex("gfx/hud/inv/torsoa"), 0),
+	new Box(new Coord( 50,   0), Resource.loadtex("gfx/hud/inv/keys"),   0),
+	new Box(new Coord( 50, 210), Resource.loadtex("gfx/hud/inv/belt"),   0),
+	new Box(new Coord( 25, 140), Resource.loadtex("gfx/hud/inv/lhande"), 0),
+	new Box(new Coord(275, 140), Resource.loadtex("gfx/hud/inv/rhande"), 0),
 	null,
-	new Coord(0, 0),
-	new Coord(0, 210),
-	new Coord(300, 0),
-	new Coord(300, 210),
-	new Coord(100, 0),
-	new Coord(0, 70),
-	new Coord(250, 210),
+	new Box(new Coord(  0,   0), Resource.loadtex("gfx/hud/inv/wallet"), 0),
+	new Box(new Coord(  0, 210), Resource.loadtex("gfx/hud/inv/coat"),   0),
+	new Box(new Coord(300,   0), Resource.loadtex("gfx/hud/inv/cape"),   0),
+	new Box(new Coord(300, 210), Resource.loadtex("gfx/hud/inv/pants"),  0),
+	new Box(new Coord(100,   0), null, 0),
+	new Box(new Coord(  0,  70), Resource.loadtex("gfx/hud/inv/back"),   0),
+	new Box(new Coord(250, 210), Resource.loadtex("gfx/hud/inv/feet"),   0),
+
+	new Box(new Coord(250,   0), Resource.loadtex("gfx/hud/inv/costumehead"),   1),
+	new Box(new Coord(50,   70), Resource.loadtex("gfx/hud/inv/costumeface"),   1),
+	new Box(new Coord(250,  70), Resource.loadtex("gfx/hud/inv/costumeshirt"),  1),
+	new Box(new Coord(300,  70), Resource.loadtex("gfx/hud/inv/costumetorsoa"), 1),
+ 	new Box(new Coord(0,   210), Resource.loadtex("gfx/hud/inv/costumecoat"),   1),
+	new Box(new Coord(300,   0), Resource.loadtex("gfx/hud/inv/costumecape"),   1),
+ 	new Box(new Coord(300, 210), Resource.loadtex("gfx/hud/inv/costumepants"),  1),
+ 	new Box(new Coord(250, 210), Resource.loadtex("gfx/hud/inv/costumefeet"),   1),
     };
-    static Tex ebgs[] = {
-	Resource.loadtex("gfx/hud/inv/head"),
-	Resource.loadtex("gfx/hud/inv/face"),
-	Resource.loadtex("gfx/hud/inv/shirt"),
-	Resource.loadtex("gfx/hud/inv/torsoa"),
-	Resource.loadtex("gfx/hud/inv/keys"),
-	Resource.loadtex("gfx/hud/inv/belt"),
-	Resource.loadtex("gfx/hud/inv/lhande"),
-	Resource.loadtex("gfx/hud/inv/rhande"),
-	null,
-	Resource.loadtex("gfx/hud/inv/wallet"),
-	Resource.loadtex("gfx/hud/inv/coat"),
-	Resource.loadtex("gfx/hud/inv/cape"),
-	Resource.loadtex("gfx/hud/inv/pants"),
-	null,
-	Resource.loadtex("gfx/hud/inv/back"),
-	Resource.loadtex("gfx/hud/inv/feet"),
-    };
-    static Coord isz;
-    static {
-	isz = new Coord();
-	for(Coord ec : ecoords) {
-	    if(ec == null)
+    public static final Coord isz = isz();
+    public final Widget[] tabs = new Widget[2];
+    public final WItem[] slots = new WItem[boxen.length];
+    private final Map<GItem, WItem[]> wmap = new HashMap<GItem, WItem[]>();
+
+    private static Coord isz() {
+	Coord isz = new Coord();
+	for(Box box : boxen) {
+	    if(box == null)
 		continue;
-	    if(ec.x + sqlite.sz().x > isz.x)
-		isz.x = ec.x + sqlite.sz().x;
-	    if(ec.y + sqlite.sz().y > isz.y)
-		isz.y = ec.y + sqlite.sz().y;
+	    if(box.c.x + sqlite.sz().x > isz.x)
+		isz.x = box.c.x + sqlite.sz().x;
+	    if(box.c.y + sqlite.sz().y > isz.y)
+		isz.y = box.c.y + sqlite.sz().y;
+	}
+	return(isz);
+    }
+
+    public static class Box {
+	public final Coord c;
+	public final Tex bg;
+	public final int tab;
+
+	public Box(Coord c, Tex bg, int tab) {
+	    this.c = c;
+	    this.bg = bg;
+	    this.tab = tab;
 	}
     }
-    WItem[] slots = new WItem[ecoords.length];
-    Map<GItem, WItem[]> wmap = new HashMap<GItem, WItem[]>();
-	
+
     @RName("epry")
     public static class $_ implements Factory {
 	public Widget create(Coord c, Widget parent, Object[] args) {
@@ -94,19 +100,42 @@ public class Equipory extends Widget implements DTarget {
 	}
     }
 
-    private class Boxen extends Widget {
-	private Boxen() {
-	    super(Coord.z, isz, Equipory.this);
+    private class Boxen extends Widget implements DTarget {
+	final int tab;
+
+	private Boxen(Coord c, Widget parent, int tab) {
+	    super(c, isz, parent);
+	    this.tab = tab;
 	}
 
 	public void draw(GOut g) {
-	    for(int i = 0; i < ecoords.length; i++) {
-		if(ecoords[i] == null)
+	    for(int i = 0; i < boxen.length; i++) {
+		Box box = boxen[i];
+		if((box == null) || (box.tab != this.tab))
 		    continue;
-		g.image(sqlite, ecoords[i]);
-		if((slots[i] == null) && (ebgs[i] != null))
-		    g.image(ebgs[i], ecoords[i].add(sqlo));
+		g.image(sqlite, box.c);
+		if((slots[i] == null) && (box.bg != null))
+		    g.image(box.bg, box.c.add(sqlo));
 	    }
+	}
+
+	public boolean drop(Coord cc, Coord ul) {
+	    ul = ul.add(sqlite.sz().div(2));
+	    for(int i = 0; i < boxen.length; i++) {
+		Box box = boxen[i];
+		if((box == null) || (box.tab != this.tab))
+		    continue;
+		if(ul.isect(box.c, sqlite.sz())) {
+		    Equipory.this.wdgmsg("drop", i);
+		    return(true);
+		}
+	    }
+	    Equipory.this.wdgmsg("drop", -1);
+	    return(true);
+	}
+
+	public boolean iteminteract(Coord cc, Coord ul) {
+	    return(false);
 	}
     }
 
@@ -119,9 +148,45 @@ public class Equipory extends Widget implements DTarget {
 
 		protected java.awt.Color clearcolor() {return(null);}
 	    };
-	new Boxen();
-    }
+	int bx = 0;
+	String s1 = "Equipment";
+	String s2 = "Costume";
+	String s3;
 	
+	
+	for(int i = 0; i < tabs.length; i++) {
+	    tabs[i] = new Widget(Coord.z, this.sz, this);
+	    tabs[i].show(i == 0);
+	    new Boxen(Coord.z, tabs[i], i);
+	    final int t = i;
+	    /*
+	    new Button(new Coord(bx, isz.y + 5), 40, this, "Tab " + (i + 1)) {
+		public void click() {
+		    for(int i = 0; i < tabs.length; i++)
+			tabs[i].show(i == t);
+		}
+	    };
+	    */
+	    if(t > 0)
+		s3 = s2;
+	    else
+		s3 = s1;
+	    
+	    Widget btn = new Button(new Coord(bx, isz.y + 5), 60, this, s3) { //"Tab " + (i + 1)) {
+		    public void click() {
+			for(int i = 0; i < tabs.length; i++)
+			    tabs[i].show(i == t);
+		    }
+		};
+	    if(t > 0)
+		btn.tooltip = Text.render("Costume");
+	    else
+		btn.tooltip = Text.render("Equipment");
+	    bx = btn.c.x + btn.sz.x + 228;
+	}
+	pack();
+    }
+
     public Widget makechild(String type, Object[] pargs, Object[] cargs) {
 	Widget ret = gettype(type).create(Coord.z, this, cargs);
 	if(ret instanceof GItem) {
@@ -129,13 +194,14 @@ public class Equipory extends Widget implements DTarget {
 	    WItem[] v = new WItem[pargs.length];
 	    for(int i = 0; i < pargs.length; i++) {
 		int ep = (Integer)pargs[i];
-		slots[ep] = v[i] = new WItem(ecoords[ep].add(sqlo), this, g);
+		Box box = boxen[ep];
+		slots[ep] = v[i] = new WItem(box.c.add(sqlo), tabs[box.tab], g);
 	    }
 	    wmap.put(g, v);
 	}
 	return(ret);
     }
-    
+
     public void cdestroy(Widget w) {
 	super.cdestroy(w);
 	if(w instanceof GItem) {
@@ -148,23 +214,5 @@ public class Equipory extends Widget implements DTarget {
 		}
 	    }
 	}
-    }
-    
-    public boolean drop(Coord cc, Coord ul) {
-	ul = ul.add(sqlite.sz().div(2));
-	for(int i = 0; i < ecoords.length; i++) {
-	    if(ecoords[i] == null)
-		continue;
-	    if(ul.isect(ecoords[i], sqlite.sz())) {
-		wdgmsg("drop", i);
-		return(true);
-	    }
-	}
-	wdgmsg("drop", -1);
-	return(true);
-    }
-    
-    public boolean iteminteract(Coord cc, Coord ul) {
-	return(false);
     }
 }
